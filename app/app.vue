b58c3cf21f9320dca41ceec019f1f46654ac1706
benchik <wygndo@gmail.com>
2026-01-25 21:08:07 +0300

    add meta tags

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
 
+
+import {SITE_DESCRIPTION, SITE_TITLE} from "~/constants/meta";
+
+useSeoMeta({
+  title: SITE_TITLE,
+  ogTitle: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+  ogDescription: SITE_DESCRIPTION,
+  charset: 'utf-8',
+})
+
 </script>
+
 <template>
   <NuxtLayout>
     <NuxtPage/>
